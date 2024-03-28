7222cae241190b20a223e41aa6645bbf2ef0557d
seandngu <nsean@seas.upenn.edu>
2024-03-27 21:38:50 -0400

    outputs for powershell to use

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,5 +1,7 @@
 output "ec2_information" {
   value = {
-    public_ip = module.ec2_instance.public_ip
+    public_ip   = module.ec2_instance.public_ip
+    domain_name = module.ec2_instance.public_dns
+    key_name    = module.ec2_instance.key_name
   }
 }
